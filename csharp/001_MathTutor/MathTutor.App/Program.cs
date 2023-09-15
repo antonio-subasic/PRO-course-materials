40453af81a1dc2b87ed78612b8e41ea0aa13d248
Antonio Subašić <antonio.subasic.as@gmail.com>
2023-09-15 15:16:31 +0200

    add csharp/001

---
+++ csharp/001_MathTutor/MathTutor.App/Program.cs
@@ -0,0 +1,6 @@
+﻿using MathTutor.Logic;
+
+var x = 10;
+var y = 20;
+var result = BasicOperations.Add(x, y);
+Console.WriteLine(result);
---
+++ csharp/001_MathTutor/MathTutor.Logic/BasicOperations.cs
@@ -0,0 +1,8 @@
+﻿namespace MathTutor.Logic;
+
+public static class BasicOperations
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+    public static int Multiply(int a, int b) => a * b;
+}
