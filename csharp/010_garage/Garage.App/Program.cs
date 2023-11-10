29e976d2d32f4051b0e95a9fa29d7972ae596cca
Antonio Subašić <antonio.subasic.as@gmail.com>
2023-11-10 14:08:30 +0100

    add csharp/010

---
+++ csharp/010_garage/Garage.App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+const int CAR_ENTRY = 1;
+const int CAR_EXIT = 2;
+const int REPORT = 3;
+const int EXIT = 4;
+
+var garage = Garage.Logic.Garage.Load("garage.json");
+
+Console.WriteLine($@"What do you want to do?
+{CAR_ENTRY}) Enter a car entry
+{CAR_EXIT}) Enter a car exit
+{REPORT}) Generate report
+{EXIT}) Exit");
+
+while (true)
+{
+    var selection = GetInputFromUser<int>("\nYour selection");
+    var invalidInput = selection == EXIT;
+    var parkingSpot = 0;
+
+    if (selection is CAR_ENTRY or CAR_EXIT)
+    {
+        parkingSpot = GetInputFromUser<int>("\nEnter parking spot number");
+        invalidInput = invalidInput || (garage.IsOccupied(parkingSpot) ^ (selection != CAR_ENTRY));
+    }
+
+    switch (selection)
+    {
+        case CAR_ENTRY:
+            if (invalidInput) { Console.WriteLine("Parking spot is occupied"); }
+            else
+            {
+                Console.Write($"Enter license plate: ");
+                var licensePlate = Console.ReadLine()!;
+                var entryDateTime = GetInputFromUser<DateTime>("Enter entry date/time");
+
+                garage.Occupy(parkingSpot, licensePlate, entryDateTime);
+            }
+            break;
+
+        case CAR_EXIT:
+            if (invalidInput) { Console.WriteLine("Parking spot is not occupied"); }
+            else
+            {
+                var exitDateTime = GetInputFromUser<DateTime>("Enter exit date/time");
+                Console.WriteLine($"Costs are {garage.Exit(parkingSpot, exitDateTime)}€");
+            }
+            break;
+
+        case REPORT:
+            Console.WriteLine($"\n{garage}");
+            break;
+
+        case EXIT:
+            Console.WriteLine("\nGood bye!");
+            garage.Save("garage.json");
+            return;
+    }
+}
+
+static T GetInputFromUser<T>(string message) where T : IParsable<T>
+{
+    Console.Write($"{message}: ");
+    var input = Console.ReadLine()!;
+    return T.Parse(input, CultureInfo.InvariantCulture);
+}
---
+++ csharp/010_garage/Garage.Logic/Garage.cs
@@ -0,0 +1,118 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Garage.Logic;
+
+public record ParkingSpot(string LicensePlate, DateTime EntryDate);
+
+public class ParkingSpotJsonRepresentation
+{
+    [JsonProperty("spot-number")]
+    public int Number { get; set; }
+
+    [JsonProperty("license-plate")]
+    public string LicensePlate { get; set; } = "";
+
+    [JsonProperty("entry-date")]
+    public DateTimeOffset EntryDate { get; set; }
+}
+
+public class Garage
+{
+    public ParkingSpot?[] ParkingSpots { get; } = new ParkingSpot[50];
+
+    private static string CenterString(string? str, int length)
+    {
+        var padding = Math.Max(length - (str?.Length ?? 0), 0) / 2d;
+
+        var before = new string(' ', (int)Math.Ceiling(padding));
+        var after = new string(' ', (int)padding);
+
+        return $"{before}{str?.Trim()}{after}";
+    }
+
+    public bool IsOccupied(int parkingSpotNumber) => ParkingSpots[parkingSpotNumber - 1] != null;
+
+    public void Occupy(int parkingSpotNumber, string licensePlate, DateTime entryTime)
+    {
+        ParkingSpots[parkingSpotNumber - 1] = new(licensePlate, entryTime);
+    }
+
+    public decimal Exit(int parkingSpotNumber, DateTime exitTime)
+    {
+        var minutes = (exitTime - ParkingSpots[parkingSpotNumber - 1]!.EntryDate).TotalMinutes;
+        ParkingSpots[parkingSpotNumber - 1] = null;
+        return minutes < 15 ? 0 : (decimal)Math.Ceiling(minutes / 30) * 3;
+    }
+
+    public string GenerateReport()
+    {
+        var maxSpotLength = Math.Max(ParkingSpots.Length.ToString().Length, "Spot".Length);
+        var maxLicensePlateLength = Math.Max(ParkingSpots.Max(parkingSpot => parkingSpot?.LicensePlate.Length ?? 0), "License Plate".Length);
+
+        var stringBuilder = new StringBuilder($"| {CenterString("Spot", maxSpotLength)} | {CenterString("License Plate", maxLicensePlateLength)} |");
+        stringBuilder.AppendLine($"\n| {new string('-', maxSpotLength)} | {new string('-', maxLicensePlateLength)} |");
+
+        for (var i = 0; i < ParkingSpots.Length; i++)
+        {
+            stringBuilder.AppendLine($"| {CenterString((i + 1).ToString(), maxSpotLength)} | {CenterString(ParkingSpots[i]?.LicensePlate, maxLicensePlateLength)} |");
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    public void Save(string filename)
+    {
+        var parkingSpots = (ParkingSpotJsonRepresentation[])this;
+        var json = JsonConvert.SerializeObject(parkingSpots, Formatting.Indented);
+        File.WriteAllText(filename, json);
+    }
+
+    public static Garage Load(string filename = "")
+    {
+        try
+        {
+            var json = File.ReadAllText(filename);
+            var parkingSpots = JsonConvert.DeserializeObject<ParkingSpotJsonRepresentation[]>(json);
+            return (Garage)parkingSpots;
+        }
+        catch (FileNotFoundException) { return new Garage(); }
+    }
+
+    public override string ToString() => GenerateReport();
+
+    public static explicit operator ParkingSpotJsonRepresentation[](Garage garage)
+    {
+        var garageJsonRepresentation = new List<ParkingSpotJsonRepresentation>();
+
+        for (var i = 0; i < garage.ParkingSpots.Length; i++)
+        {
+            if (garage.ParkingSpots[i] is not null)
+            {
+                garageJsonRepresentation.Add(new ParkingSpotJsonRepresentation
+                {
+                    Number = i + 1,
+                    LicensePlate = garage.ParkingSpots[i]!.LicensePlate,
+                    EntryDate = garage.ParkingSpots[i]!.EntryDate
+                });
+            }
+        }
+
+        return garageJsonRepresentation.ToArray();
+    }
+
+    public static explicit operator Garage(ParkingSpotJsonRepresentation[]? parkingSpots)
+    {
+        var garage = new Garage();
+
+        if (parkingSpots is not null)
+        {
+            foreach (var parkingSpot in parkingSpots)
+            {
+                garage.Occupy(parkingSpot.Number, parkingSpot.LicensePlate, parkingSpot.EntryDate.DateTime);
+            }
+        }
+
+        return garage;
+    }
+}
