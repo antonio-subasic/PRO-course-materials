1db4ffd75a1ef899ee1ab41deafb9f6d566217ee
Antonio Subašić <antonio.subasic.as@gmail.com>
2023-11-17 10:46:56 +0100

    add csharp/012

---
+++ csharp/012_restaurant_queue/RestaurantQueue.App/Program.cs
@@ -0,0 +1,72 @@
+﻿using RestaurantQueue.Logic;
+
+#region Choice Options
+const int ADD = 1;
+const int SEAT = 2;
+const int DISPLAY = 3;
+const int SAVE = 4;
+const int LOAD = 5;
+const int REMOVE = 6;
+#endregion
+
+#region Choice Options Output
+Console.WriteLine("""
+What do you want to do?
+1) Add a customer to the queue
+2) Seat the next customer
+3) Display the queue
+4) Save the queue to queue.json
+5) Load the queue from queue.json
+6) Remove customer from the queue
+""");
+#endregion
+
+var restaurantQueue = new RestaurantQueue.Logic.RestaurantQueue();
+
+do
+{
+    Console.Write("\nYour choice: ");
+    var choice = int.Parse(Console.ReadLine()!);
+
+    switch (choice)
+    {
+        case ADD:
+            {
+                Console.Write("Enter customer name: ");
+                var name = Console.ReadLine()!;
+
+                Console.Write("Enter customer phone number: ");
+                var phoneNumber = Console.ReadLine()!;
+
+                restaurantQueue.Add(new CustomerInQueue(name, phoneNumber));
+                break;
+            }
+
+        case SEAT:
+            var customer = restaurantQueue.Remove();
+            if (customer is not null) { Console.WriteLine($"Seating {customer}"); }
+            break;
+
+        case DISPLAY:
+            Console.WriteLine(restaurantQueue);
+            break;
+
+        case SAVE:
+            Console.WriteLine(restaurantQueue.Save("queue.json"));
+            return;
+
+        case LOAD:
+            var output = restaurantQueue.TryLoad("queue.json");
+            if (output is not null) { Console.WriteLine(output); }
+            break;
+
+        case REMOVE:
+            {
+                Console.Write("Enter customer name: ");
+                var name = Console.ReadLine()!;
+
+                restaurantQueue.RemoveByName(name);
+                break;
+            }
+    }
+} while (!restaurantQueue.IsEmpty);
---
+++ csharp/012_restaurant_queue/RestaurantQueue.Logic/Restaurant.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RestaurantQueue.Logic;
+
+public class RestaurantQueue
+{
+    private CustomerInQueue? _first { get; set; }
+    private CustomerInQueue? _last { get; set; }
+    public bool IsEmpty => _first is null || _last is null;
+
+    public void Add(CustomerInQueue? customer)
+    {
+        if (IsEmpty) { _first = _last = customer; }
+        else
+        {
+            Debug.Assert(_last is not null, "If IsEmpty is false, _last must not be null");
+            _last = _last.Next = customer;
+        }
+    }
+
+    public CustomerInQueue? Remove()
+    {
+        var current = _first;
+
+        if (_first is not null) { _first = _first.Next; }
+        if (_first is null) { _last = null; }
+
+        return current;
+    }
+
+    public void RemoveByName(string name)
+    {
+        if (_first?.Name == name) { Remove(); }
+        else
+        {
+            var current = _first;
+
+            while (current?.Next?.Name != name && current?.Next is not null) { current = current.Next; }
+            if (current?.Next is not null) { current.Next = current.Next.Next; }
+        }
+    }
+
+    public string Save(string filename)
+    {
+        var customers = new List<CustomerInQueue>();
+
+        for (var current = _first; current is not null; current = current.Next) { customers.Add(current); }
+
+        File.WriteAllText(
+            filename,
+            JsonSerializer.Serialize(customers, new JsonSerializerOptions() { WriteIndented = true })
+        );
+
+        return $"Saved {customers.Count} customers to {filename}";
+    }
+
+    public string? TryLoad(string filename)
+    {
+        var customers = JsonSerializer.Deserialize<CustomerInQueue[]>(File.ReadAllText(filename));
+
+        if (customers is not null)
+        {
+            foreach (var customer in customers) { Add(customer); }
+            return $"Loaded {customers.Length} customers from {filename}";
+        }
+        else { return null; }
+    }
+
+    public override string? ToString()
+    {
+        var stringBuilder = new StringBuilder();
+
+        for (var current = _first; current is not null; current = current.Next)
+        {
+            stringBuilder.AppendLine(current.ToString());
+        }
+
+        return stringBuilder.Length == 0 ? "The queue is empty" : stringBuilder.ToString().TrimEnd('\n');
+    }
+}
+
+public class CustomerInQueue(string name, string phoneNumber)
+{
+    [JsonPropertyName("name")]
+    public string Name { get; } = name;
+
+    [JsonPropertyName("phone-number")]
+    public string PhoneNumber { get; } = phoneNumber;
+
+    [JsonIgnore]
+    public CustomerInQueue? Next { get; set; }
+
+    public override string ToString() => $"{Name} ({PhoneNumber})";
+}
