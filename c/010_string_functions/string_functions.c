8cd53e5dd6c5463547c0786ee8f2c09f3b2c285d
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-01-10 14:12:38 +0100

    updated c/010 added custom string-function: - contains - trim

--- c/010_string_functions/string_functions.c
+++ c/010_string_functions/string_functions.c
@@ -42,6 +42,43 @@ int mystrcmp(char *first, char *second)
     return 0;
 }
 
+int mystrcontains(char *haystack, char *needle)
+{
+    int needleLength = mystrlen(needle);
+    int haystackLength = mystrlen(haystack);
+
+    for (int i = 0; i < haystackLength; i++)
+    {
+        for (int j = 0; j < needleLength; j++)
+        {
+            if (haystack[i + j] != needle[j]) { break; }
+
+            if (j == needleLength - 1)
+            {
+                // it contains the string so return 0
+                return 0;
+            }
+        }
+    }
+
+    // it doesn't contain the string so return -1
+    return -1;
+}
+
+int mystrtrim(char *dest, char *src)
+{
+    int start = 0;
+    while (start < mystrlen(src) && src[start] == ' ') { start++; }
+
+    int end = mystrlen(src) - 1;
+    while (end > 0 && src[end] == ' ') { end--; }
+
+    for (int i = 0; i <= end - start; i++)
+    {
+        dest[i] = src[i + start];
+    }
+}
+
 int main()
 {
     char str1[50] = "Hello";
@@ -57,7 +94,17 @@ int main()
     printf("str4: %s\n", str4);
 
     int difference = mystrcmp(str1, str2);
-    printf("str1 %s str2", difference < 0 ? "is smaller than" : difference > 0 ? "is bigger than" : "is equal to");
+    printf("str1 %s str2\n", difference < 0 ? "is smaller than" : difference > 0 ? "is bigger than" : "is equal to");
+
+    printf("str2 contains str1: %s\n", mystrcontains(str2, str1) == 0 ? "true" : "false");
+
+    printf("str1 contains str3: %s\n", mystrcontains(str1, str3) == 0 ? "true" : "false");
+
+    char str5[50] = "   Hello World   ";
+    char str6[50];
+    mystrtrim(str6, str5);
+    printf("str5: '%s'\n", str5);
+    printf("str6: '%s'\n", str6);
 
     return 0;
 }
