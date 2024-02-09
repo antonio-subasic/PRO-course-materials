82fbe295dc4ee04b512904fdb93341f90677285b
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-02-09 08:52:51 +0100

    added csharp/021

---
+++ csharp/021_hashsets/Program.cs
@@ -0,0 +1,20 @@
+﻿HashSet<int> numbers = [1, 2, 3, 4, 5, 5];
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+if (numbers.Contains(3))
+{
+    Console.WriteLine("yes");
+}
+
+HashSet<int> otherNumbers = [1, 2, 3, 4, 5];
+
+if (numbers.SetEquals(otherNumbers))
+{
+    Console.WriteLine("equal");
+}
+
+// Console.WriteLine("8".GetHashCode());
