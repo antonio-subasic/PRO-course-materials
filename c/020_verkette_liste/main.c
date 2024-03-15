2368d4d153f4ed057b3b426b5720859f2f0d535b
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-03-15 13:08:01 +0100

    added c/020

---
+++ c/020_verkette_liste/main.c
@@ -0,0 +1,90 @@
+/*
+-------------------------------------------------------------
+                    HTBLA Leonding - 2AHIF
+-------------------------------------------------------------
+                 Antonio Subašić, 15.03.2024
+-------------------------------------------------------------
+                        Verkette Liste
+-------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element {
+  int daten;
+  struct element *next;
+};
+
+struct element *anfang = NULL;
+
+void printList() {
+  struct element *aktuell = anfang;
+  while (aktuell != NULL) {
+    printf("%d ", aktuell->daten);
+    aktuell = aktuell->next;
+  }
+  printf("\n");
+}
+
+void append(int wert) {
+  struct element *neu = (struct element *)malloc(sizeof(struct element));
+  if (neu == NULL) {
+    printf("Speicher konnte nicht allokiert werden.\n");
+    return;
+  }
+  neu->daten = wert;
+  neu->next = NULL;
+
+  if (anfang == NULL) {
+    anfang = neu;
+    return;
+  }
+
+  struct element *aktuell = anfang;
+  while (aktuell->next != NULL) {
+    aktuell = aktuell->next;
+  }
+  aktuell->next = neu;
+}
+
+void deleteTail() {
+  if (anfang == NULL) {
+    printf("Die Liste ist bereits leer.\n");
+    return;
+  }
+
+  if (anfang->next == NULL) {
+    free(anfang);
+    anfang = NULL;
+    return;
+  }
+
+  struct element *vorletztes = anfang;
+  while (vorletztes->next->next != NULL) {
+    vorletztes = vorletztes->next;
+  }
+
+  free(vorletztes->next);
+  vorletztes->next = NULL;
+}
+
+int main() {
+  append(1);
+  append(2);
+  append(3);
+  printList();
+
+  deleteTail();
+  printList();
+
+  deleteTail();
+  printList();
+
+  deleteTail();
+  printList();
+
+  deleteTail();
+
+  return 0;
+}
