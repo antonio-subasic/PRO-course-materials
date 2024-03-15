c206ce964d059b1f744164395b5f599100420389
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-03-15 09:09:34 +0100

    added csharp/027

---
+++ csharp/027_figlet/Program.cs
@@ -0,0 +1,47 @@
+﻿using WenceyWang.FIGlet;
+
+var rendereres = new[]
+{
+    new TextRenderer(),
+    new FigletRenderer("fonts/Bloody.flf"),
+};
+
+foreach (var renderer in rendereres)
+{
+    var text = new[] { "Hello World" };
+    var renderedText = renderer.ReaderText(text);
+    Console.WriteLine(string.Join("\n", renderedText));
+}
+
+class TextRenderer
+{
+    public virtual string[] ReaderText(string[] text) => text;
+}
+
+class FigletRenderer : TextRenderer
+{
+    private readonly FIGletFont? font;
+
+    public FigletRenderer(string? fontPath = null)
+    {
+        if (fontPath is not null)
+        {
+            using var stream = File.OpenRead(fontPath);
+            font = new FIGletFont(stream);
+        }
+    }
+
+    public override string[] ReaderText(string[] text)
+    {
+        var result = new List<string>();
+
+        foreach (var line in text)
+        {
+            var art = new AsciiArt(line, font);
+            result.AddRange(art.ToString().Split('\n'));
+        }
+
+        return [.. result];
+    }
+}
+
