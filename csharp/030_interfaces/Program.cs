b7f023e54e53a20b64b194479ab9e66c9691112e
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-04-05 08:42:42 +0200

    added csharp/030

---
+++ csharp/030_interfaces/Program.cs
@@ -0,0 +1,52 @@
+﻿// var mh = new IMiceHunter(); -> not possible
+// IMiceHunter mh = new Owl(); -> possible
+
+Animal[] zoo = [
+    new Cat(),
+    new Owl(),
+    new Kiwi()
+];
+
+List<IMiceHunter> hunters = [
+    new Cat(),
+    new Owl()
+];
+
+HuntMice(hunters);
+
+void HuntMice(IEnumerable<IMiceHunter> hunters)
+{
+    foreach (var hunter in hunters)
+    {
+        hunter.CatchMouse();
+    }
+}
+
+abstract class Animal { }
+
+abstract class Bird : Animal
+{
+    public virtual bool CanFly => true;
+}
+
+interface IMiceHunter
+{
+    void CatchMouse();
+    void Something() => Console.WriteLine("something");
+}
+
+class Owl : Bird, IMiceHunter
+{
+    public void CatchMouse() => Console.WriteLine("watch - fly - catch");
+}
+
+class Kiwi : Bird
+{
+    public override bool CanFly => false;
+}
+
+class Cat : Animal, IMiceHunter
+{
+    public void CatchMouse() => Console.WriteLine("watch - jump - catch");
+    public void SayMiau() => Console.WriteLine("Miau");
+}
